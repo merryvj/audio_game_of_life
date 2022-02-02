7b2fff1b740074cbbf34404dfd450c33b70c6724
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-02-02 18:13:28 -0500

    beats are sick

--- audio.js
+++ audio.js
@@ -1,4 +1,62 @@
 const sound1 = new Audio("./sounds/boom.wav");
-function playSounds() {
-    sound1.play();
+const sound2 = new Audio("./sounds/clap.wav");
+const sound3 = new Audio("./sounds/hihat.wav");
+const sound4 = new Audio("./sounds/kick.wav");
+const sound5 = new Audio("./sounds/ride.wav");
+const sound6 = new Audio("./sounds/snare.wav");
+const sound7 = new Audio("./sounds/tink.wav");
+const sound8 = new Audio("./sounds/tom.wav");
+
+function playSounds(states) {
+    
+    let sounds = [];
+    states.forEach(state => {
+        switch(state) {
+            case 1:
+                sounds.push(sound1);
+            case 2:
+                sounds.push(sound2);
+            case 3:
+                sounds.push(sound3);
+            case 4:
+                sounds.push(sound4);
+            case 5:
+                sounds.push(sound5);
+            case 6:
+                sounds.push(sound6);
+            case 7:
+                sounds.push(sound7);
+            case 8:
+                sounds.push(sound8);
+                
+        }
+    })
+
+    // cells.forEach((rowArr, row) => {
+    //     rowArr.forEach((colVal, col) => {
+    //         switch(col) {
+    //             case 1:
+    //                 sounds.push(sound1);
+    //             case 2:
+    //                 sounds.push(sound2);
+    //             case 3:
+    //                 sounds.push(sound3);
+    //             case 4:
+    //                 sounds.push(sound4);
+    //             case 5:
+    //                 sounds.push(sound5);
+    //             case 6:
+    //                 sounds.push(sound6);
+    //             case 7:
+    //                 sounds.push(sound7);
+    //             case 8:
+    //                 sounds.push(sound8);
+    //         }
+            
+    //     });
+    // });
+
+    sounds.forEach(sound => {
+        sound.play();
+    })
 }
--- main.js
+++ main.js
@@ -1,121 +1,128 @@
-// import {draw as drawGrid, update as updateGrid, toggleCell as cellClicked, update} from "./grid.js"
-// import {playSounds} from "./audio";
 const grid = document.getElementById("grid");
 const stepBtn = document.getElementById("step-btn");
-let cells = [
-]
-const gridSz = 16;
+let cells = [];
+let soundStates = [];
+const gridSz = 8;
+let cellsEl = null;
+let interval;
 
 init();
 
-function init() {
-    initGrid();
-    render();
+function start() {
+  interval = setInterval(update, 300);
 }
+
+
 function render() {
-    grid.innerHTML = '';
-    grid.appendChild(draw());
+  grid.innerHTML = "";
+  cellsEl = draw();
+  grid.appendChild(cellsEl);
 }
 
-stepBtn.addEventListener('click', () => {
-    update();
-    render();
-    playSounds();
-})
-
-grid.addEventListener('click', e => {
-    if (e.target.tagName == 'TD') {
-        let cell = e.target;
-        let row = cell.getAttribute("r");
-        let col = cell.getAttribute("c");
-        toggleCell(row, col);
-    }
-    render();
-})
-
-
-
-
+grid.addEventListener("click", (e) => {
+  if (e.target.tagName == "TD") {
+    let cell = e.target;
+    let row = cell.getAttribute("r");
+    let col = cell.getAttribute("c");
+    toggleCell(row, col);
+  }
+  render();
+});
 
 //populate grid with 0
-function initGrid() {
-    for (let r = 0; r < gridSz; r++){
-        let row = [];
-        for (let c = 0; c < gridSz; c++){
-            row.push(0);
-        }
-
-        cells.push(row);
+function init() {
+  cells = [];
+  for (let r = 0; r < gridSz; r++) {
+    let row = [];
+    for (let c = 0; c < gridSz; c++) {
+      row.push(0);
     }
+
+    cells.push(row);
+  }
+
+  render();
 }
 
-
 function toggleCell(row, col) {
-    cells[row][col] = -1 * cells[row][col] + 1;
+  cells[row][col] = -1 * cells[row][col] + 1;
 }
 
 function draw() {
-    let table = document.createElement("table");
-    cells.forEach((rowArr, row) => {
-        let rowEl = document.createElement("tr");
-        rowArr.forEach((colVal, col) => {
-          let cell = document.createElement("td");
-          cell.setAttribute("c", col);
-          cell.setAttribute("r", row);
-          cell.classList.add("_" + colVal);
-          rowEl.append(cell);
-        });
-
-        table.append(rowEl);
+  let table = document.createElement("table");
+  cells.forEach((rowArr, row) => {
+    let rowEl = document.createElement("tr");
+    rowArr.forEach((colVal, col) => {
+      let cell = document.createElement("td");
+      cell.setAttribute("c", col);
+      cell.setAttribute("r", row);
+      if (colVal > 0) {
+        cell.classList.add("_" + col);
+        soundStates.push(col);
+      }
+      rowEl.append(cell);
     });
 
-    return table;
+    table.append(rowEl);
+  });
+
+  return table;
 }
 
 function update() {
-    let newCells = []; // Upcoming grid
-        cells.forEach((rowArr, row) => {
-          let newRow = [];
-          rowArr.forEach((colVal, col) => {
-            let cellVal = colVal;
-            let nCount = checkNeighbors(row, col);
-            if (cellVal == 1 && nCount < 2) { // If live and <2 live neighbors
-              cellVal = 0;
-            } else if (cellVal == 1 && nCount > 3) { // If live and >3 live neighbors
-              cellVal = 0;
-            } else if (cellVal == 0 && nCount == 3) { // If dead and 3 live neighbors
-              cellVal = 1;
-            }
+  let newCells = []; // Upcoming grid
+  cells.forEach((rowArr, row) => {
+    let newRow = [];
+    rowArr.forEach((colVal, col) => {
+      let cellVal = colVal;
+      let nCount = checkNeighbors(row, col);
+      if (cellVal == 1 && nCount < 2) {
+        // If live and <2 live neighbors
+        cellVal = 0;
+      } else if (cellVal == 1 && nCount > 3) {
+        // If live and >3 live neighbors
+        cellVal = 0;
+      } else if (cellVal == 0 && nCount == 3) {
+        // If dead and 3 live neighbors
+        cellVal = 1;
+      }
 
-            newRow.push(cellVal);
-          });
-          newCells.push(newRow);
-        });
-        cells = newCells; // Update the grid
+      newRow.push(cellVal);
+    });
+    newCells.push(newRow);
+  });
+  cells = newCells; // Update the grid
+  render();
+  playSounds(soundStates);
+  soundStates = [];
 }
 
 function checkNeighbors(row, col) {
-    // Return number of live neighbors
-    
-    let count = 0;
-    
-    for (let i = -1; i < 2; i++) { //This checks the row above and row below
-      if (col + i >= 0 && col + i < gridSz - 1) { // Check for valid column
-        if (row > 0 && cells[row - 1][col + i] == 1) {
-          count++;
-        }
-        if (row < gridSz - 1 && cells[row + 1][col + i] == 1) { 
-          count++;
-        }
+  // Return number of live neighbors
+
+  let count = 0;
+
+  for (let i = -1; i < 2; i++) {
+    //This checks the row above and row below
+    if (col + i >= 0 && col + i < gridSz - 1) {
+      // Check for valid column
+      if (row > 0 && cells[row - 1][col + i] == 1) {
+        count++;
+      }
+      if (row < gridSz - 1 && cells[row + 1][col + i] == 1) {
+        count++;
       }
     }
-    
-    if (col - 1 >= 0 && cells[row][col - 1] == 1) { // Check left cell
-      count++;
-    }
-    if (col + 1 < gridSz - 1 && cells[row][col + 1] == 1) { // Check right cell
-      count++;
-    }
-
-    return count;
   }
+
+  if (col - 1 >= 0 && cells[row][col - 1] == 1) {
+    // Check left cell
+    count++;
+  }
+  if (col + 1 < gridSz - 1 && cells[row][col + 1] == 1) {
+    // Check right cell
+    count++;
+  }
+
+  return count;
+}
